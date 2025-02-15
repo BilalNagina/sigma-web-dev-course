7b8ec61934f908c18f3b5726b2e293281b9c1ecd
Bilal Nagina <154643382+BilalNagina@users.noreply.github.com>
2025-02-15 10:21:28 +0300

    loops & functions

---
+++ JS/Loops & Functions in Js/script.js
@@ -0,0 +1,63 @@
+console.log("Welcome to the class for Loops & Functions");
+const func = (x) => {
+    console.log("Hello World " + x);
+}
+func(5);
+func(65);
+func(33);
+// let a = 0;
+// do {
+//     console.log("Hello");
+//     a++;
+// } while (a < 3);
+// let a = 5;
+// for (let i = 0; i < 10; i++) {
+//     console.log(a + i);
+// }
+// question 1 
+/*
+const obj = {
+  gaet: 57,
+  jhes: 29,
+  bilal: 90
+};
+const keys = Object.keys(obj);
+for (let i = 0; i < keys.length; i++) {  
+    const element = obj[keys[i]];
+    console.log(keys[i] + ":" + element);
+}
+*/
+// question 2
+// const obj = {
+//     gaet: 57,
+//     jhes: 29,
+//     bilal: 90
+//   };
+// for (const key in obj)  {
+//         const element = obj[key];
+//         console.log(key + ": " + element); 
+    
+// }
+// question 3
+// const correctNumber = 7; // Set the correct number
+// let userInput = 4
+// while (userInput != correctNumber) {
+//         console.log("Try again!");  
+// }
+// question 4
+// function mean (num){
+//     let sum=0
+//     for (let i = 0; i < num.length; i++) {
+//         const element = num[i];
+//     sum += element 
+//     }
+//     return sum / num.length;
+// }
+// console.log(mean([1, 2, 3]));
+
+// function mean (a,b,c,d,e){
+//     let sum=0
+//     sum = a + b + c + d + e
+//     return sum/5
+// }
+// console.log(mean(1,2,3,4,5))
