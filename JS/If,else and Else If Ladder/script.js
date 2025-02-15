d70608795fb519a3d3930f07e092bf836cbe0a08
Bilal Nagina <154643382+BilalNagina@users.noreply.github.com>
2025-02-15 08:32:25 +0300

    If,else and data types concepts

---
+++ JS/If,else and Else If Ladder/script.js
@@ -0,0 +1,42 @@
+console.log('Hello, world!');
+let age = 6;
+switch(age){
+    case 1:
+        console.log('age is 1');
+        break;
+    case 2:
+        console.log('age is 2');
+        break;
+    case 3:
+        console.log('age is 3');
+        break;
+    default:
+        console.log('age is not 1, 2, or 3');
+}
+let b = 5;
+let c = 6;
+let d = b+c;
+console.log(d);
+// let c = (age > 18) ? "You Can Drive" : "You Can't Drive";
+// console.log(c);
+// // age = 32;
+// if (age > 10 && age < 20 ) {
+//     console.log('Age is between 10 and 20');
+// } else if (age > 20 && age < 30) {
+//     console.log('Age is not between 10 and 20');
+// }
+// else {
+//     console.log('Age is not valid');
+// }
+// let a = 3;
+// if(a%2 == 0 || a%3 == 0){
+//     console.log('Number is divisible by 2 or 3');
+// }else{
+//     console.log('Number is not divisible by 2 or 3');
+// }
+// let a = 3;
+// if(a%2 == 0 && a%3 == 0){
+//     console.log('Number is divisible by 2 and 3');
+// }else{
+//     console.log('Number is not divisible by 2 and 3');
+// }
---
+++ JS/JS Var, Data Types & Objects/test.js
@@ -0,0 +1,35 @@
+// Question one:
+// var a = "bilal"
+// a = a + 10
+// console.log(a)
+// Output: "bilal10"
+// Explanation: In the above code, we have declared a variable a with a string value "bilal". Then we have added 10 to the string value. In JavaScript, when we add a number to a string, it concatenates the number to the string. So the output will be "bilal10".
+
+// Question two:
+// console.log(typeof a)
+// Output: string
+
+// Question three:
+const a = {
+  name: "bilal",
+  age: 20,
+};
+// a = 10
+// output: TypeError: It has already been declared
+
+// Question four:
+a.father = "bilal ahmed"
+console.log(a)
+// output: { name: 'bilal', age: 20, father: 'bilal ahmed' }
+// ex
+
+// Question five:
+const dictionary = {
+    name: "bilal",
+    right: "true",
+    wrong: "false",
+    hate: "dislike",
+    love: "like",
+    };
+    console.log(dictionary);
+// explanation: In the above code, we have declared a constant dictionary with key-value pairs. We have added some key-value pairs to the dictionary object. Then we have printed the dictionary object using console.log(). The output will be the dictionary object with all the key-value pairs.
