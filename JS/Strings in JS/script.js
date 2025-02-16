db281bd9cb0b00b27fd1d67cc13fbcf80987d412
Bilal Nagina <154643382+BilalNagina@users.noreply.github.com>
2025-02-16 14:53:54 +0300

    Strings

---
+++ JS/Strings in JS/script.js
@@ -0,0 +1,14 @@
+console.log("I'm learning Strings in JavaScript!");
+let b = "BiLal Asif";
+console.log("har\"".length);
+// console.log("har\"".charAt(3));
+console.log(b.length)
+// console.log(b.startsWith("b"))
+// console.log(b.endsWith("al"))
+// console.log(b.includes("l"))
+// console.log(b.toLowerCase())
+// console.log(b.trim())
+console.log(b.replace("a","A"))
+let c = " Please give me Rs 1000"
+console.log(c.slice(18,22))
+console.log(b.concat(c, " Thank you"))
