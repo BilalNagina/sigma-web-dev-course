c69e15e5ff3a59c565680c16583cfee488240d55
Bilal Nagina <154643382+BilalNagina@users.noreply.github.com>
2025-02-19 09:58:45 +0300

    Arrays practice

---
+++ JS/Arrays in JS/script.js
@@ -0,0 +1,74 @@
+console.log("Hello, world!");
+
+// question 1
+// let a = [1, 2, 3, 4, 5];
+// let b = prompt('Enter a number: ');
+// a.push(b);
+// console.log(a);
+
+// question 2
+// let a = [1, 2, 3, 4, 5];
+
+// let b;
+// while (true) {
+//   b = Number(prompt("Enter a positive number (Except Zero): "));
+
+//   if (b > 0) {
+//     a.push(b);
+//     console.log(a);
+//   } else if (b == 0) {
+//     console.log("You entered zero. Please enter a positive number");
+//     break;
+//   } else {
+//     console.log("Enter a valid number");
+//   }
+// }
+
+// question 3
+
+// let a = [10, 15, 120];
+// let b = Number(prompt('Enter a number: '));
+// a.push(b);
+// console.log(a);
+// function ten(a) {
+//     for (let i = 0; i < a.length; i++) {
+//         if (a[i] % 10 == 0) {
+//             console.log(a[i]);
+//         }
+//     }
+// }
+// ten(a);
+
+// filter method
+// let c = [10, 1, 2, 3, 15, 13];
+// const small = (e) => {
+//   if (e > 5) {
+//     return true;
+//   }
+//   return false;
+// };
+// console.log(c.filter(small));
+
+// question 4
+
+// let a = [10, 15, 13];
+// let newa = [];
+// for (let i = 0; i < a.length; i++) {
+//     newa.push(a[i] ** 2);
+// }
+// console.log(newa);
+// let b = a.map((e) => {
+//   return e ** 2;
+// });
+// console.log(b);
+
+// question 5
+
+// let a = [1,2,3,4,5];
+// const func = (a,n) =>{
+//     return a*n
+// }
+// console.log(a.reduce(func));
+
+
+
