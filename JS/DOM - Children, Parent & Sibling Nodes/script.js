e4f673dc57d3eb29e63d7b27e0668716fc8e9963
Bilal Nagina <154643382+BilalNagina@users.noreply.github.com>
2025-09-22 21:42:07 +0530

    update

--- JS/DOM - Children, Parent & Sibling Nodes/script.js
+++ JS/DOM - Children, Parent & Sibling Nodes/script.js
@@ -1,4 +1,12 @@
 console.log("Hello, World!");
 //question 1
-let nav = document.body.firstElementChild.childNodes[1].firstElementChild
-nav.firstElementChild.style.color = "red";
+// let nav = document.body.firstElementChild.childNodes[1].firstElementChild
+// nav.firstElementChild.style.color = "red";
+
+// let head = document.body.childNodes[3].firstElementChild.childNodes[1]
+// head.lastElementChild.style.color = "blue"
+// head.firstElementChild.style.color = "blue"
+
+// let nav = document.body.childNodes[3].firstElementChild.childNodes[1]
+// nav.style.color = "cyan"
+
