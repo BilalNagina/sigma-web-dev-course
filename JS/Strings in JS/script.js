61f4515aea31711f057cc8f3501e140ca356868f
Bilal Nagina <154643382+BilalNagina@users.noreply.github.com>
2025-10-02 10:18:26 +0530

    file updated

--- JS/Strings in JS/script.js
+++ JS/Strings in JS/script.js
@@ -3,12 +3,15 @@ let b = "BiLal Asif";
 console.log("har\"".length);
 // console.log("har\"".charAt(3));
 console.log(b.length)
+let real_name = "Raihan";
+let real_friend = "Anas";
+console.log(`The names are ${real_name} and ${real_friend}!!`)
 // console.log(b.startsWith("b"))
 // console.log(b.endsWith("al"))
 // console.log(b.includes("l"))
 // console.log(b.toLowerCase())
 // console.log(b.trim())
-console.log(b.replace("a","A"))
+console.log(b.replace("a","707"))
 let c = " Please give me Rs 1000"
-console.log(c.slice(18,22))
-console.log(b.concat(c, " Thank you"))
+console.log(c.slice(1,14));
+console.log(b.concat(c, " Thank you", " Anas"));
