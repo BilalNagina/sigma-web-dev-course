69f02f85c6a3ec9216271d28ea9257f905047d1f
Bilal Nagina <154643382+BilalNagina@users.noreply.github.com>
2025-10-02 09:39:39 +0530

    file updated

--- Projects/Faulty Calculator/script.js
+++ Projects/Faulty Calculator/script.js
@@ -1,23 +1,20 @@
-console.log("Faulty Calculator");
-let a = prompt("Enter the first number: ");
-let c = prompt("Enter the operator: ");
-let b = prompt("Enter the second number: ");
-
+let a = prompt("Enter the first Number: ");
+let b = prompt("Enter your operator: ");
+let c = prompt("Enter the Second Number: ");
 
 let random = Math.random();
-let obj = {
-    "+": "-",
-    "*": "+",
-    "-": "/",
-    "/": "**"
-}
-if (random > 0.1) {
-// correct calculation
-    console.log(`The result is ${a} ${c} ${b}`);
-    alert(`The result is ${eval(`${a} ${c} ${b}`)}`);
-} else {
-    // faulty calculation
-    c = obj[c];
-    alert(`The result is ${eval(`${a} ${c} ${b}`)}`);
-}
 
+let obj = {
+  "+": "-",
+  "*": "+",
+  "-": "/",
+  "/": "**",
+};
+
+if (random > 0.1) {
+  console.log(`The Result of ${a} ${b} ${c}`);
+  alert(`The result is ${eval(`${a} ${b} ${c}`)}`);
+} else {
+   b = obj[b];
+   alert(`The Result is ${eval(`${a} ${b} ${c}`)}`);
+}
