ba7f36d9df7ef8046fa7b5ba10493a9155d35852
Bilal Nagina <154643382+BilalNagina@users.noreply.github.com>
2025-10-15 12:44:50 +0530

    Exercise Solutions -ch8

---
+++ JS/DOM - Insert & Delete Elements/Question1.js
@@ -0,0 +1,24 @@
+// console.log("script is initializing.....");
+// let btn1 = document.querySelector(".btn1");
+// let btn2 = document.querySelector(".btn2");
+// let btn3 = document.querySelector(".btn3");
+
+// btn1.addEventListener("click", function () {
+//   alert("Button One is clicked!");
+// });
+
+// btn2.addEventListener("click", function () {
+//   alert("Button Two is clicked!");
+// });
+
+// btn3.addEventListener("click", function () {
+//   alert("Button Three is clicked!");
+// });
+
+document.querySelectorAll("button").forEach((xyz, ibsd) => {
+  xyz.addEventListener("click", () => {
+    alert(`Button ${ibsd + 1} is clicked!`);
+  });
+});
+
+
